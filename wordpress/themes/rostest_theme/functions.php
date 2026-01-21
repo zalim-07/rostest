718efaa2ebf338d4a7c56c56dacc4646f6e530c4
Zalim <tatrokovzm@gmail.com>
2026-01-21 18:19:34 +0300

    Task 2: Add specialization/city taxonomies and ACF fields

--- wordpress/themes/rostest_theme/functions.php
+++ wordpress/themes/rostest_theme/functions.php
@@ -45,3 +45,54 @@ function rostest_register_doctors_cpt()
     ));
 }
 add_action('init', 'rostest_register_doctors_cpt');
+
+/* Регистрация Taxonomies для докторов */
+function rostest_register_doctors_taxonomies()
+{
+    register_taxonomy('specialization', 'doctors', array(
+        'labels' => array(
+            'name' => __('Специализации', 'rostest_theme'),
+            'singular_name' => __('Специализация', 'rostest_theme'),
+            'search_items' => __('Искать специализации', 'rostest_theme'),
+            'all_items' => __('Все специализации', 'rostest_theme'),
+            'edit_item' => __('Редактировать специализацию', 'rostest_theme'),
+            'update_item' => __('Обновить специализацию', 'rostest_theme'),
+            'add_new_item' => __('Добавить новую специализацию', 'rostest_theme'),
+            'new_item_name' => __('Название новой специализации', 'rostest_theme'),
+        ),
+        'hierarchical' => true,
+        'public' => true,
+        'show_admin_column' => true,
+        'rewrite' => array('slug' => 'specialization'),
+    ));
+
+    register_taxonomy('city', 'doctors', array(
+        'labels' => array(
+            'name' => __('Города', 'rostest_theme'),
+            'singular_name' => __('Город', 'rostest_theme'),
+            'search_items' => __('Искать города', 'rostest_theme'),
+            'all_items' => __('Все города', 'rostest_theme'),
+            'edit_item' => __('Редактировать город', 'rostest_theme'),
+            'update_item' => __('Обновить город', 'rostest_theme'),
+            'add_new_item' => __('Добавить новый город', 'rostest_theme'),
+            'new_item_name' => __('Название нового города', 'rostest_theme'),
+        ),
+        'hierarchical' => true,
+        'public' => true,
+        'show_admin_column' => true,
+        'rewrite' => array('slug' => 'city'),
+    ));
+}
+add_action('init', 'rostest_register_doctors_taxonomies');
+
+
+/* ACF JSON */
+add_filter('acf/settings/load_json', function ($paths) {
+    unset($paths[0]); // убираем стандартный путь
+    $paths[] = get_stylesheet_directory() . '/acf-json';
+    return $paths;
+});
+
+add_filter('acf/settings/save_json', function () {
+    return get_stylesheet_directory() . '/acf-json';
+});
